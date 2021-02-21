a0a36468d0dd8af9a38957516d386859d0bab20b
mikaela.pisani <mikaela.pisani@rootstrap.com>
2021-02-21 16:43:13 -0300

    fix: add val size 0 in predicting

--- modules/predicting.py
+++ modules/predicting.py
@@ -17,19 +17,19 @@ class PredictingModule():
 
   def predict_probabilities(self, model, X_train, X_val, y_train, y_val, X_test, y_test):
     y_train_pred = model.predict_proba(X_train)
-    y_pred = model.predict_proba(X_test)
-    y_val_pred = model.predict_proba(X_val)
-
     df_train_pred = pd.DataFrame()
     df_train_pred['probability'] = y_train_pred[:,1]
     df_train_pred['predicted_value'] = df_train_pred['probability'].apply(lambda x: 1 if x>0.5 else 0)
     df_train_pred['real_value'] = y_train
 
     df_val_pred = pd.DataFrame()
-    df_val_pred['probability'] = y_val_pred[:,1]
-    df_val_pred['predicted_value'] = df_val_pred['probability'].apply(lambda x: 1 if x>0.5 else 0)
-    df_val_pred['real_value'] = y_val
+    if (X_val!=[]):
+        y_val_pred = model.predict_proba(X_val)
+        df_val_pred['probability'] = y_val_pred[:,1]
+        df_val_pred['predicted_value'] = df_val_pred['probability'].apply(lambda x: 1 if x>0.5 else 0)
+        df_val_pred['real_value'] = y_val
 
+    y_pred = model.predict_proba(X_test)
     df_test_pred = pd.DataFrame()
     df_test_pred['probability'] = y_pred[:,1]
     df_test_pred['predicted_value'] = df_test_pred['probability'].apply(lambda x: 1 if x>0.5 else 0)
